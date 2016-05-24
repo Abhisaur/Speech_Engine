41059dcda78d3c716dd53ffe7fc06e86024d0ab1
mrinaald <mrinaald@iitk.ac.in>
2016-05-24 23:00:08 +0530

    Write audio to a WAV file.

--- Speech_recog.py
+++ Speech_recog.py
@@ -9,6 +9,10 @@ with sr.Microphone() as source:
     r.pause_threshold = 0.5
     audio = r.listen(source)
 
+	# write audio to a WAV file
+with open("microphone-results.wav", "wb") as f:
+    f.write(audio.get_wav_data())
+    
 # recognize speech using Sphinx
 try:
     print("Sphinx thinks you said = " + r.recognize_sphinx(audio))
